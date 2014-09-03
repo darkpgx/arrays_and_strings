5622b3742fe1f834183f78c611874fc1a2cffba9
darkpgx <darkpgx@gmail.com>
2014-09-03 10:16:12 -0700

    replace for loop with string split method to put string into array

--- reverse_words.js
+++ reverse_words.js
@@ -3,19 +3,8 @@
 //Assume that all words are space delimited and treat punctuation the same as letters.
 
 var reverseWords = function (str) {
-  var words = [];
-  var word_holder = '';
+  var words = str.split(' '); // put string into array delimiting by space
   var reverse_str = '';
-  for (var i = 0; i < str.length; i++) {
-    if(str.charAt(i) !== ' ') {
-      word_holder = word_holder + str.charAt(i);
-    }
-    else {
-      words.push(word_holder);
-      word_holder ='';
-    };
-  };
-  if (word_holder !== '') {words.push(word_holder);}; //for the last word that has not been added to array
   for (var i = words.length -1; i >= 0; i--) {
     if (i !== 0) {
       reverse_str = reverse_str + words[i] + ' ';
