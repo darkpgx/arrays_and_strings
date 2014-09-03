c10c57998dc407e435774cbaad5337564015eb5b
darkpgx <darkpgx@gmail.com>
2014-09-03 11:27:24 -0700

    add regexp to remove_chars2.js

--- remove_chars2.js
+++ remove_chars2.js
@@ -1,13 +1,9 @@
 //improvement for remove_chars.js by using the actual string itself and indexOf instead of lookup table
+//using regular expressions to simplify the function
 
 var remove_chars = function(str, remove) {
-  var temp = ''; //temporary string to add only the necessary chars when going through the loop once
-  for(var i = 0; i < str.length; i++) {
-    if (remove.indexOf(str.charAt(i)) == -1) {
-      temp = temp + str.charAt(i);
-    };
-  };
-  return temp;
+  var pattern = new RegExp ("[" + remove + "]","g");
+  return str.replace(pattern, '');
 };
 
 console.log(remove_chars('hello', 'eo'));
